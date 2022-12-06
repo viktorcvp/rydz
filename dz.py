3c9ae14158dc3f08786327c136153c9997b38f32
“viktorcvp” <dffggg2013@yandex.by>
2022-12-06 15:57:37 +0300

    dz_5

--- dz.py
+++ dz.py
@@ -189,3 +189,99 @@
 # with open("file34_res.txt", "r") as data:
 #     st3 = data.readlines()
 # print(f"Результирующий многочлен {st3}")
+# 5_1 вариант человек против человека:
+# from random import randint
+
+
+# def input_dat(name):
+#     x = int(input(f"{name}, введите количество конфет, которое возьмете от 1 до 28: "))
+#     while x < 1 or x > 28:
+#         x = int(input(f"{name}, введите корректное количество конфет: "))
+#     return x
+
+
+# def p_print(name, k, counter, value):
+#     print(
+#         f"Ходил {name}, он взял {k}, теперь у него {counter}. Осталось на столе {value} конфет."
+#     )
+
+
+# player1 = input("Введите имя первого игрока: ")
+# player2 = input("Введите имя второго игрока: ")
+# value = int(input("Введите количество конфет на столе: "))
+# flag = randint(0, 2)  # флаг очередности
+# if flag:
+#     print(f"Первый ходит {player1}")
+# else:
+#     print(f"Первый ходит {player2}")
+
+# counter1 = 0
+# counter2 = 0
+
+# while value > 28:
+#     if flag:
+#         k = input_dat(player1)
+#         counter1 += k
+#         value -= k
+#         flag = False
+#         p_print(player1, k, counter1, value)
+#     else:
+#         k = input_dat(player2)
+#         counter2 += k
+#         value -= k
+#         flag = True
+#         p_print(player2, k, counter2, value)
+
+# if flag:
+#     print(f"Выиграл {player1}")
+# else:
+#     print(f"Выиграл {player2}")
+
+# 5_1
+# # вариант человек против бота:
+# from random import randint
+
+
+# def input_dat(name):
+#     x = int(input(f"{name}, введите количество конфет, которое возьмете от 1 до 28: "))
+#     while x < 1 or x > 28:
+#         x = int(input(f"{name}, введите корректное количество конфет: "))
+#     return x
+
+
+# def p_print(name, k, counter, value):
+#     print(
+#         f"Ходил {name}, он взял {k}, теперь у него {counter}. Осталось на столе {value} конфет."
+#     )
+
+
+# player1 = input("Введите имя первого игрока: ")
+# player2 = "Bot"
+# value = int(input("Введите количество конфет на столе: "))
+# flag = randint(0, 2)  # флаг очередности
+# if flag:
+#     print(f"Первый ходит {player1}")
+# else:
+#     print(f"Первый ходит {player2}")
+
+# counter1 = 0
+# counter2 = 0
+
+# while value > 28:
+#     if flag:
+#         k = input_dat(player1)
+#         counter1 += k
+#         value -= k
+#         flag = False
+#         p_print(player1, k, counter1, value)
+#     else:
+#         k = randint(1, 29)
+#         counter2 += k
+#         value -= k
+#         flag = True
+#         p_print(player2, k, counter2, value)
+
+# if flag:
+#     print(f"Выиграл {player1}")
+# else:
+#     print(f"Выиграл {player2}")
